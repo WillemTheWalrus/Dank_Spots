fb3c7c3b7cb1e781b184d1fd8fa6bcbcb9a32a54
Unknown <billdoudna@gmail.com>
2018-04-08 19:11:55 -0700

    map and marker ui functional
    Add functionality to the checkboxes, figured out how to move the map camera, and can now display marker description and title when the marker is clicked.

--- practice/markerObject and aracata locations classes/src/main/java/com/willemthewalrus/mapsui/MapsActivity.java
+++ practice/markerObject and aracata locations classes/src/main/java/com/willemthewalrus/mapsui/MapsActivity.java
@@ -2,8 +2,10 @@ package com.willemthewalrus.mapsui;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,6 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -32,16 +35,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         arcataLocations.populateLocations();
     }
 
-    public void onClick(View view){
-        mMap.addMarker(new MarkerOptions().position(new LatLng(40.877500, -124.087148)).title("new marker"));
-    }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     *
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -49,6 +48,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                for(int i = 0; i < mapMarkers.size(); i++){
+                    if(marker.getTitle().equals(mapMarkers.get(i).getTitle())){
+                        markerObject currentmarker = markerList.get(i);
+                        TextView description = findViewById(R.id.description);
+                        description.setText(currentmarker.getSnippet());
+                        Log.i("markerclick", "markerfound");
+                        marker.showInfoWindow();
+                        return true;
+
+                    }
+                }
+                Log.i("markerclick","markernotfound");
+                return false;
+            }
+        });
 
         for(int i= 0; i < markerList.size(); i++){
             mapMarkers.add(mMap.addMarker(new MarkerOptions().position(markerList.get(i).getPosition()).title(markerList.get(i).getTitle())));
@@ -56,9 +73,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.870977, -124.086185), (float)15));
+
     }
 
     public void plugFilter(View view){
@@ -81,8 +98,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
 
+    }
+
+    public void spotFilter(View view){
+
+        CheckBox spots  = findViewById(R.id.spots);
+        boolean isChecked = spots.isChecked();
+
+        if(isChecked){
+            for(int i = 0; i < markerList.size(); i++){
+                if(markerList.get(i).getType()== 2 ){
+                    mapMarkers.get(i).setVisible(true);
+                }
+            }
+        }
+        else{
+            for(int i = 0; i < markerList.size(); i++){
+                if(markerList.get(i).getType()== 2){
+                    mapMarkers.get(i).setVisible(false);
+                }
+            }
+        }
 
     }
+
+    public void munchiesFilter(View view){
+        CheckBox munchies  = findViewById(R.id.munchies);
+        boolean isChecked = munchies.isChecked();
+
+        if(isChecked){
+            for(int i = 0; i < markerList.size(); i++){
+                if(markerList.get(i).getType()==0){
+                    mapMarkers.get(i).setVisible(true);
+                }
+            }
+        }
+        else{
+            for(int i = 0; i < markerList.size(); i++){
+                if(markerList.get(i).getType()==0){
+                    mapMarkers.get(i).setVisible(false);
+                }
+            }
+        }
+    }
+
+
 
 
 }
