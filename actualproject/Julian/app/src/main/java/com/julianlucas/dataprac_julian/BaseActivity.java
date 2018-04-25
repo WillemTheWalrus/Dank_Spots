62c5190edd2cbc4e0a5a25c94a769a7f8e408ec8
JulesLu <jjl438@humboldt.edu>
2018-04-25 13:54:27 -0700

    getting location functions "completed"

--- actualproject/Julian/app/src/main/java/com/julianlucas/dataprac_julian/BaseActivity.java
+++ actualproject/Julian/app/src/main/java/com/julianlucas/dataprac_julian/BaseActivity.java
@@ -1,22 +1,32 @@
 package com.julianlucas.dataprac_julian;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.MenuItem;
 
 import java.util.List;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.Collection;
 
-public abstract class BaseActivity extends FragmentActivity implements OnMapReadyCallback {
+public abstract class BaseActivity extends FragmentActivity implements OnMapReadyCallback, LocationProvider.LocationCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
     public static GoogleMap mMap;
+    private LocationProvider mLocationProvider;
+    public int REQUEST_PERMISSIONS_ACCESS;
 
 
     protected int getLayoutId() {
@@ -28,6 +38,7 @@ public abstract class BaseActivity extends FragmentActivity implements OnMapRead
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         setUpMap();
+        mLocationProvider.connect();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
@@ -35,7 +46,7 @@ public abstract class BaseActivity extends FragmentActivity implements OnMapRead
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         item.setChecked(true);
-                        if(item.getItemId() == R.id.mySpots){
+                        if (item.getItemId() == R.id.mySpots) {
 
                         }
                         return true;
@@ -49,6 +60,26 @@ public abstract class BaseActivity extends FragmentActivity implements OnMapRead
     protected void onResume() {
         super.onResume();
         setUpMap();
+        mLocationProvider.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+
+    public void handleNewLocation(Location location) {
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.setMinZoomPreference(15.0f);
+        mMap.setMaxZoomPreference(23.0f);
     }
 
     @Override
@@ -57,11 +88,36 @@ public abstract class BaseActivity extends FragmentActivity implements OnMapRead
             return;
         }
         mMap = map;
-        startMap();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Camera permission has not been granted.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_ACCESS);
+
+        } else {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+            startMap();
+        }
     }
+
+
 
     private void setUpMap() {
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Camera permission has not been granted.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_ACCESS);
+
+        } else {
+
+            mLocationProvider = new LocationProvider(this, this);
+        }
     }
 
     /**
@@ -74,7 +130,20 @@ public abstract class BaseActivity extends FragmentActivity implements OnMapRead
     }
 
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
 }
 
 
